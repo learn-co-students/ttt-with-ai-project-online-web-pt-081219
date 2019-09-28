cc74fcaf5fde8fbd22b94468f7728bf5c73dae4e
Katie Seggelke <kathrynseggelke@Kathryn-Kempers-MacBook-Pro.local>
2019-09-28 15:47:02 -0600

    Done.

---
+++ lib/board.rb
@@ -0,0 +1,13 @@
+class Board 
+    attr_accessor :cells  
+        
+    def initialize 
+        reset! 
+    end 
+
+    def reset!
+        @cells = ["","","","","","","","","",""]
+    end  
+
+
+end 
